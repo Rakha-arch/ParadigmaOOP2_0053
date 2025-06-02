95af0fe0a3b6edb656b811e9b6067b064423cb12
Rakha-arch <miftahuzahran@gmail.com>
2025-06-02 09:55:17 +0700

    first commit

---
+++ abstraksi/abstraksi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class abstraksiklas {
+    private: string x, y;
+
+    public:
+
+        //method untuk mengisi nilai
+        //private member
+        void setXY(string a, string b){
+            x = a;
+            y = b;
+        }
+}
